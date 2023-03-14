fda6baf20602c4b4d3daf965fe4af6b6923bf812
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-03-14 10:20:14 -0300

    criada classe projeto

--- codigo/Projeto 1/Gestao De Estoque/src/Produto.java
+++ codigo/Projeto 1/Gestao De Estoque/src/Produto.java
@@ -1,6 +1,84 @@
-
 public class Produto {
+	private String descricao;
+	private float precoCusto;
+	private int margemLucro;
+	private int quantidade;
+	private int quantidadeMinima;
 
+	public Produto(String descricao, float precoCusto, int margemLucro, int quantidade) {
+		this.descricao = descricao;
+		this.precoCusto = precoCusto;
+		this.quantidade = quantidade;
+		if (margemLucro < 30 ) {
+			this.margemLucro = 30;
+		} else if (margemLucro  > 80) {
+			this.margemLucro = 80;
+		} else {
+			this.margemLucro = margemLucro;
+		}
+	}
+
+	public Produto(String descricao, float precoCusto, int margemLucro) {
+		this(descricao, precoCusto, margemLucro, 0);
+	}
+
+	public Produto(String descricao, float precoCusto) {
+		this(descricao, precoCusto, 30);
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(float precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+
+	public int getMargemLucro() {
+		return margemLucro;
+	}
+
+	public void setMargemLucro(int margemLucro) {
+		if (margemLucro < 30 ) {
+			this.margemLucro = 30;
+		} else if (margemLucro  > 80) {
+			this.margemLucro = 80;
+		} else {
+			this.margemLucro = margemLucro;
+		}
+		this.margemLucro = margemLucro;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
 	
-	
+	public float getPrecoVenda() {
+		return ((margemLucro / 100) + 1) * precoCusto + this.getImposto();
+	}
+
+	public float getImposto() {
+		return ((margemLucro / 100.0f) + 1) * precoCusto * 0.18f;
+	}
+
+	public void setQuantidadeMinima(int quantidadeMinima) {
+		this.quantidadeMinima = quantidadeMinima;
+	}
+
+	public boolean temSuficiente() {
+		return quantidade > quantidadeMinima;
+	}
+
 }
