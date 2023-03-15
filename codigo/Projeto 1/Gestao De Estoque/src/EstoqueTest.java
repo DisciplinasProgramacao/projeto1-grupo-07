84a245a9ccdbc51ed8ecf970b1ce8157216412bd
joaoqueiroga <queiroga.joaofcsol@gmail.com>
2023-03-15 20:25:18 -0300

    corrigidos os testes

--- codigo/Projeto 1/Gestao De Estoque/src/EstoqueTest.java
+++ codigo/Projeto 1/Gestao De Estoque/src/EstoqueTest.java
@@ -1,36 +1,33 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Before;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class EstoqueTest {
 	
 	private Estoque estoque;
-	
-	@Before
+
+	@BeforeEach
 	public void setUp() {
 		estoque = new Estoque();
 
-		Produto produto1 = new Produto("Produto 1", 10.0f, 50, 20);
-		Produto produto2 = new Produto("Produto 2", 20.0f, 30, 5);
-		Produto produto3 = new Produto("Produto 3", 30.0f, 70, 15);
+		Produto produto1 = new Produto("Produto 1", 10.0f, 50, 20, 10);
+		Produto produto2 = new Produto("Produto 2", 20.0f, 30, 5, 10);
+		Produto produto3 = new Produto("Produto 3", 30.0f, 70, 15, 10);
 		
 		estoque.cadastraProduto(produto1);
 		estoque.cadastraProduto(produto2);
 		estoque.cadastraProduto(produto3);
 	}
-	
-	
 
 	@Test
 	public void testCadastraProduto() {
 		Produto produto4 = new Produto("Produto 4", 40.0f, 40, 25);
 		estoque.cadastraProduto(produto4);
-		
+
 		assertEquals(produto4, estoque.getProduto(3));
 	}
-	
+
 	@Test
 	public void testAddProduto() {
 		estoque.addProduto(0, 5);
@@ -51,25 +48,18 @@ public class EstoqueTest {
 		Produto produto5 = new Produto("Produto 5", 50.0f, 50, 5);
 		estoque.cadastraProduto(produto5);
 		
-		assertEquals(2, estoque.getAbaixoDoMinimo().size());
+		assertEquals(2, estoque.getAbaixoDoMinimo().lines().count());
 	}
 	
 	@Test
 	public void testRetiraDeEstoque() {
-		estoque.retiraDeEstoque(1);
-		
-		//assertEquals(2, estoque.getProduto(1).getId());
-	}
-	
-	@Test
-	public void testMostraProdutos() {
-		String expected = "0\tProduto 1\t20\n1\tProduto 2\t5\n2\tProduto 3\t15\n";
-		
-		assertEquals(expected, estoque.mostraProdutos());
+		Produto p1 = estoque.getProduto(1);
+
+		assertEquals(p1 , estoque.retiraDeEstoque(1));
 	}
 	
 	@Test
 	public void testGetValorTotal() {
-		assertEquals(1050.0, estoque.getValorTotal(), 0.1);
+		assertEquals(108.56, estoque.getValorTotal(), 0.1);
 	}
 }
